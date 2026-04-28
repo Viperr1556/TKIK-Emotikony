a8c23a01d7416effbcb1e3e3103ab47f59fd306e
BartlomiejCzosnek <bartlomiejczosnek@gmail.com>
2026-04-28 19:49:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import sys
 from parser_yacc import parser
 from ast_nodes import Environment
 
+
 def run_interpreter(filename):
     try:
         with open(filename, 'r', encoding='utf-8') as f:
             code = f.read()
-            
+
         ast = parser.parse(code)
-        
+
         if ast:
             global_env = Environment()
             ast.eval(global_env)
-            
+
     except Exception as e:
-        print(f"Wystąpił błąd: {e}")
+        print(f"🔥 Krytyczny błąd: {e}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
